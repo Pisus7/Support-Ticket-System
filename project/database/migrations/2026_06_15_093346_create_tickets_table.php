b51aa6d45b930276205680e3ca6686b28e60b2e7
pulle12 <psummerauer@tsn.at>
2026-06-15 11:43:18 +0200

    feat: create tickets migration

---
+++ project/database/migrations/2026_06_15_093346_create_tickets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('ticket_nr')->unique();
+            $table->string('ticket_subject');
+            $table->longText('ticket_message');
+            $table->string('ticket_status');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
