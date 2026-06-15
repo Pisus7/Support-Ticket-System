b580838ea112cd56bbfa5afca912fd3d2e75d2c9
pulle12 <psummerauer@tsn.at>
2026-06-15 11:50:38 +0200

    feat: add and modify model classes (also with FK methods)

---
+++ project/app/Models/Ticket.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Ticket extends Model
+{
+    protected $table = 'tickets';
+
+    protected $fillable = [
+        'user_id',
+        'ticket_nr',
+        'ticket_subject',
+        'ticket_message',
+        'ticket_status'
+    ];
+
+    protected $guarded = [];
+
+    protected $attributes = [
+        'state' => 'open'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
