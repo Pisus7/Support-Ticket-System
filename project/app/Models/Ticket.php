a697277090a91940bad10947470a1cbeb3da9f7e
pulle12 <psummerauer@tsn.at>
2026-06-16 09:31:48 +0200

    feat: update model class eloquent methods

--- project/app/Models/Ticket.php
+++ project/app/Models/Ticket.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Ticket extends Model
 {
@@ -31,5 +32,10 @@ class Ticket extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
