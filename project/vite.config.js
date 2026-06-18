e0ed83e30a45c51ee3f3684e75d1a77fd69eb7f2
pulle12 <psummerauer@tsn.at>
2026-06-18 13:44:55 +0200

    bugfix

--- project/vite.config.js
+++ project/vite.config.js
@@ -9,5 +9,16 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
+        {
+            name: 'custom-terminal-output',
+            configureServer(server) {
+                // Wartet, bis der Server gestartet ist, und gibt dann Text aus
+                server.httpServer?.once('listening', () => {
+                    setTimeout(() => {
+                        console.log('\n  \x1b[35m➜\x1b[0m  \x1b[1mMailpit Dashboard\x1b[0m: \x1b[36mhttp://localhost:8025\x1b[0m');
+                    }, 100); // Kurzer Timeout, damit es nach den Standard-Vite-Links kommt
+                });
+            }
+        }
     ],
 });
