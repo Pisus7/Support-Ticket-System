3d489c2905941de34162604bbcd00a650360c58d
piusrauch <pirauch@tsn.at>
2026-06-19 09:23:28 +0200

    navigation für tickets eingebaut

--- project/resources/js/Layouts/AuthenticatedLayout.jsx
+++ project/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -30,6 +30,12 @@ export default function AuthenticatedLayout({ header, children }) {
                                 >
                                     Dashboard
                                 </NavLink>
+                                <NavLink
+                                    href={route('tickets.index')}
+                                    active={route().current('tickets.*')}
+                                >
+                                    Tickets
+                                </NavLink>
                             </div>
                         </div>
 
